b84b3e2cf7da566adc7063dc62ce8e3d223d1ef0
Victor Navarro <victor.navarro.xyz@gmail.com>
2024-11-14 23:48:13 +0100

    fix: grid-de-imagenes image link

--- components/module/GridDeImagenes.vue
+++ components/module/GridDeImagenes.vue
@@ -3,34 +3,40 @@ import type { GridDeImagenesModule } from '#imports';
 
 
 const prop = defineProps<{
-    m: GridDeImagenesModule
+  m: GridDeImagenesModule
 }>();
 </script>
 
 
 <template>
-    <section class="c-grid-de-imagenes">
-        <div class="inner | boxed">
-            <h2 class="t-title" v-if="!isEmpty(m.props.title.value)">
-                {{ m.props.title.value }}
-            </h2>
-            <div class="body | rich-text"
-                v-if="!isEmpty(m.props.text.value)"
-                v-html="m.props.text.value"
-            />
-            <div class="image-grid">
-                <component class="image-block"
-                    :class="`is-${c.props.width.getOption().value}-cols`"
-                    v-for="(c, i) in m.props.images.value" :key="i"
-                    :is="!isEmpty(c.props.link.value)? 'a' : 'div'"
-                    :href="stringOrDefault(c.props.link.value, null)"
-                    :target="c.props.link.value? '_blank' : null"
-                    >
-                    <BuilderImage :alt="c.props.alt.value" />
-                </component>
-            </div>
+  <section class="c-grid-de-imagenes">
+    <div class="inner | boxed">
+      <h2 class="t-title" v-if="!isEmpty(m.props.title.value)">
+        {{ m.props.title.value }}
+      </h2>
+      <div class="body | rich-text"
+      v-if="!isEmpty(m.props.text.value)"
+      v-html="m.props.text.value"
+      />
+      <div class="image-grid">
+        <!-- <component class="image-block"
+        :class="`is-${c.props.width.getOption().value}-cols`"
+        v-for="(c, i) in m.props.images.value" :key="i"
+        :is="!isEmpty(c.props.link.value)? 'a' : 'div'"
+        :href="stringOrDefault(c.props.link.value, null)"
+        :target="c.props.link.value? '_blank' : null"
+        >
+          <BuilderImage :alt="c.props.alt.value" />
+        </component> -->
+        <div class="image-block"
+        v-for="(c, i) in m.props.images.value" :key="i"
+        :class="`is-${c.props.width.getOption().value}-cols`"
+        >
+          <BuilderImage :alt="c.props.alt.value" />
         </div>
-    </section>
+      </div>
+    </div>
+  </section>
 </template>
 
 
--- utils/modules/gridDeImagenes.ts
+++ utils/modules/gridDeImagenes.ts
@@ -2,63 +2,63 @@ import { Module, SubModule } from "@/utils/moduleTypes";
 
 
 type GridDeImagenesSubPropDic = {
-    link: InstanceType<typeof ModulePropString>,
-    alt: InstanceType<typeof ModulePropString>,
-    width: InstanceType<typeof ModulePropOptions>,
+  // link: InstanceType<typeof ModulePropString>,
+  alt: InstanceType<typeof ModulePropString>,
+  width: InstanceType<typeof ModulePropOptions>,
 }
 export class GridDeImagenesSubModule extends SubModule<GridDeImagenesSubPropDic>
 {
-    createProps()
-    {
-        return {
-            link: new ModulePropString('Link URL', ''),
-            alt: new ModulePropString('Imagen alt-text', '', 'plain', 1),
-            width: new ModulePropOptions('Ancho', [
-                { label: 'Mitad', value: '6' },
-                { label: 'Tercio', value: '4' },
-                { label: 'Cuarto', value: '3' },
-            ], 2, 1),
-        }
+  createProps()
+  {
+    return {
+      // link: new ModulePropString('Link URL', ''),
+      alt: new ModulePropString('Imagen alt-text', '', 'plain', 1),
+      width: new ModulePropOptions('Ancho', [
+        { label: 'Mitad', value: '6' },
+        { label: 'Tercio', value: '4' },
+        { label: 'Cuarto', value: '3' },
+      ], 2, 1),
     }
-    getDescriptor()
-    {
-        const link = this.props.link.value;
-        const alt = cleanHTML(this.props.alt.value);
-        if (!isEmpty(alt)) return alt;
-        else if (!isEmpty(link)) return link;
-        else return this.getTitle();
-    }
+  }
+  getDescriptor()
+  {
+    // const link = this.props.link.value;
+    const alt = cleanHTML(this.props.alt.value);
+    if (!isEmpty(alt)) return alt;
+    // else if (!isEmpty(link)) return link;
+    else return this.getTitle();
+  }
 }
 
 
 type GridDeImagenesPropDic = {
-    title: InstanceType<typeof ModulePropString>;
-    text: InstanceType<typeof ModulePropString>;
-    images: InstanceType<typeof ModulePropArray<GridDeImagenesSubModule>>;
+  title: InstanceType<typeof ModulePropString>;
+  text: InstanceType<typeof ModulePropString>;
+  images: InstanceType<typeof ModulePropArray<GridDeImagenesSubModule>>;
 }
 export class GridDeImagenesModule extends Module<GridDeImagenesPropDic>
 {
-    constructor()
-    {
-        super('grid-de-imagenes');
-        this.additionalInfo = 'Imagen de 200px de alto (ancho variable)';
+  constructor()
+  {
+    super('grid-de-imagenes');
+    this.additionalInfo = 'Imagen de 200px de alto (ancho variable)';
+  }
+  createProps()
+  {
+    return {
+      title: new ModulePropString('Título', 'Grid de imágenes'),
+      text: new ModulePropString('Texto', LONG_LOREM, 'rich-h3'),
+      images: new ModulePropArray(
+        GridDeImagenesSubModule, 'imagen', 'Imágenes'
+      ),
     }
-    createProps()
-    {
-        return {
-            title: new ModulePropString('Título', 'Grid de imágenes'),
-            text: new ModulePropString('Texto', LONG_LOREM, 'rich-h3'),
-            images: new ModulePropArray(
-                GridDeImagenesSubModule, 'imagen', 'Imágenes'
-            ),
-        }
-    }
-    getDescriptor()
-    {
-        const title = this.props.title.value;
-        const text = cleanHTML(this.props.text.value);
-        if (!isEmpty(title)) return title;
-        else if (!isEmpty(text)) return text;
-        else return this.getTitle();
-    }
+  }
+  getDescriptor()
+  {
+    const title = this.props.title.value;
+    const text = cleanHTML(this.props.text.value);
+    if (!isEmpty(title)) return title;
+    else if (!isEmpty(text)) return text;
+    else return this.getTitle();
+  }
 }
