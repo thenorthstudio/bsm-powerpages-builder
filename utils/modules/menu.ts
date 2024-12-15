0f7564a1cc5bf428b83ceadda5fafb7c07f04056
Victor Navarro <victor.navarro.xyz@gmail.com>
2024-12-16 00:46:06 +0100

    fix: make language URLs start with "https://"

--- utils/modules/menu.ts
+++ utils/modules/menu.ts
@@ -9,7 +9,11 @@ type MenuPropDic = {
 }
 export class MenuModule extends Module<MenuPropDic>
 {
-  constructor() { super('menu') }
+  constructor()
+  {
+    super('menu');
+    this.additionalInfo = 'Para el resto de idiomas, las URLs deben empezar por https://';
+  }
   createProps()
   {
     const defaultItem ={
@@ -25,7 +29,22 @@ export class MenuModule extends Module<MenuPropDic>
     props.pageLinks.additionalInfo = 'Debe empezar por /';
     return props;
   }
-  override onAnyChange() {
+  override onAnyChange()
+  {
+    // Ensure all URLs start with https://:
+    const props = ['esUrl', 'caUrl', 'enUrl'] as (keyof MenuPropDic)[];
+    for (const prop of props)
+    {
+      const url = this.props[prop].value as string;
+      const httpsPrefix = url.match(/^ht{1,2}p?s?:?\/?\/?/i)?.[0] || '';
+      if (httpsPrefix && httpsPrefix !== 'https://') {
+        this.props[prop].value = 'https://' + url.slice(httpsPrefix.length);
+      }
+      else if (!url.startsWith('https://')) {
+        this.props[prop].value = 'https://' + url;
+      }
+    }
+    // Refresh language selectors:
     useCurrentPage().dirtyJS.value = true;
   }
   getDescriptor() { return this.getTitle() }
